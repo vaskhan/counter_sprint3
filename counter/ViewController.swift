3214d6aff252660d11d366c8fd9a5ab20a761fb3
Vasiliy Khanin <ya-v.khanin@yandex.ru>
2024-10-08 17:36:42 +0300

    Внесены правки по замечаниям: исправлены наименования переменных и UI-элементов, выравнен код, прописан контроль доступа для класса, методов и свойств, восстановлена структура класса в соответствии с рекомендациями.

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -1,62 +1,63 @@
-    //
-    //  ViewController.swift
-    //  counter
-    //
-    //  Created by Василий Ханин on 06.10.2024.
-    //
 
-    import UIKit
+import UIKit
 
-    class ViewController: UIViewController {
-        @IBOutlet weak var counterField: UILabel!
-        @IBOutlet weak var textView: UITextView!
+final class ViewController: UIViewController {
+    
+    //MARK: - IB Outlets
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var changesTextView: UITextView!
+    
+    //MARK: - Private Properties
+    private var count: Int = 0
+    
+    // MARK: - View Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        private var i: Int = 0
+        changesTextView.layer.borderColor = UIColor.darkGray.cgColor
+        changesTextView.layer.borderWidth = 2.0
         
-        @IBAction func minusButton(_ sender: Any) {
-            if i == 0 {
-                textView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: попытка уменьшить значение счётчика ниже 0\n"
-            } else {
-                i -= 1
-                counterField.text = "Значение счётчика: " + String(i)
-                textView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: значение изменено на -1\n"
-            }
-            scrollToBottom()
-            
-        }
-        @IBAction func plusButton(_ sender: Any) {
-            i += 1
-            counterField.text = "Значение счётчика: " + String(i)
-            textView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: значение изменено на +1\n"
-            scrollToBottom()
-        }
-        
-        @IBAction func resetButton(_ sender: Any) {
-            i = 0
-            counterField.text = "Значение счётчика: " + String(i)
-            textView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: значение сброшено\n"
-            scrollToBottom()
-        }
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            textView.layer.borderColor = UIColor.darkGray.cgColor
-            textView.layer.borderWidth = 2.0
-            
-            counterField.layer.borderColor = UIColor.darkGray.cgColor
-            counterField.layer.borderWidth = 2.0
-            // Do any additional setup after loading the view.
-        }
-
-        func scrollToBottom() {
-            let range = NSRange(location: textView.text.count - 1, length: 1)
-            textView.scrollRangeToVisible(range)
-        }
+        counterLabel.layer.borderColor = UIColor.darkGray.cgColor
+        counterLabel.layer.borderWidth = 2.0
     }
-    extension Date {
-        func formatted(as format: String = "yyyy-MM-dd HH:mm:ss") -> String {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = format
-            return dateFormatter.string(from: self)
+    
+    // MARK: - IB Actions
+    @IBAction private func minusButton(_ sender: Any) {
+        if count == 0 {
+            changesTextView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: попытка уменьшить значение счётчика ниже 0\n"
+        } else {
+            count -= 1
+            counterLabel.text = "Значение счётчика: " + String(count)
+            changesTextView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: значение изменено на -1\n"
         }
+        scrollToBottom()
+        
     }
+    @IBAction private func plusButton(_ sender: Any) {
+        count += 1
+        counterLabel.text = "Значение счётчика: " + String(count)
+        changesTextView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: значение изменено на +1\n"
+        scrollToBottom()
+    }
+    
+    @IBAction private func resetButton(_ sender: Any) {
+        count = 0
+        counterLabel.text = "Значение счётчика: " + String(count)
+        changesTextView.text += "[\(Date().formatted(as: "yyyy-MM-dd HH:mm:ss"))]: значение сброшено\n"
+        scrollToBottom()
+    }
+    
+    // MARK: - Private Methods
+    private func scrollToBottom() {
+        let range = NSRange(location: changesTextView.text.count - 1, length: 1)
+        changesTextView.scrollRangeToVisible(range)
+    }
+}
+//MARK: - Extensions
+extension Date {
+    func formatted(as format: String = "yyyy-MM-dd HH:mm:ss") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
+    }
+}
